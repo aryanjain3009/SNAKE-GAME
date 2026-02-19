69356325779d5f45eb68e7fbba899ae21cb6b751
Aryan Jain <jainaryan1818@gmail.com>
2026-02-19 17:54:01 +0530

    added basic js files

---
+++ script.js
@@ -0,0 +1,36 @@
+const board = document.querySelector('.board');
+const blockheight = 50;
+const blockwidth = 50;
+
+const cols = Math.floor (board.clientWidth/blockwidth);
+const rows = Math.floor(board.clientHeight/blockheight)
+
+const blocks = [];
+const snake = [
+    {x:1,y:3},
+    {x:1,y:4},
+    {x:1,y:5}
+]
+
+// for(let i=0; i< rows*cols ; i++){
+//     const block = document.createElement('div')
+//     block.classList.add("block")
+//     board.appendChild(block);
+
+// }
+
+for(let row =0; row<rows ; row++){
+    for(let col = 0; col<cols; col++){
+        const block = document.createElement('div')
+        block.classList.add("block")
+        board.appendChild(block);
+        block.innerText = (`${row}-${col}`);
+        blocks[`${row}-${col}`] = block;
+    }
+}
+
+function render (){
+    snake.forEach(segment => {
+    blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+    })
+}
